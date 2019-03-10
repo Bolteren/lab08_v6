5aca2c31f01a91c440c81f1e78032fc761ba7f72
Bolter <BolterHell@gmail.com>
2019-03-10 16:36:00 +0500

    Структурирование. Фикс бага.

---
+++ funcshen.cpp
@@ -0,0 +1,97 @@
+#include "funcshen.hpp"
+
+
+int modl(int a)
+{
+	if (a < 0)
+	{
+		return (-1 * a);
+	}
+	return a;
+}
+
+double modl(double a)
+{
+	if (a < 0)
+	{
+		return (-1 * a);
+	}
+	return a;
+}
+
+coord vctr(coord v0, coord v1) 
+{
+	coord v1v0;
+	v1v0 = v1 - v0;
+	return v1v0;
+}
+
+double testPoint(coord v0, coord v1, coord vtest)
+{
+	double a,b,c, dtest;
+	a = v1.y - v0.y;
+	b = v0.x - v1.x;
+	c = v1.x * v0.y - v0.x * v1.y;
+	dtest = a * vtest.x + b * vtest.y + c;
+	return dtest;
+}
+
+int testArr(double *arr)
+{
+	bool tst = false;
+	double test1 = 0.0, test2 = 0.0;
+	for(int counter = 0; counter < 8; counter ++)
+	{
+		test1 += arr[counter];
+	}
+	for(int counter = 0; counter < 8; counter ++)
+	{
+		test2 += modl(arr[counter]);
+	}
+	test1 = modl(test1);
+	if(test1 == test2)
+	{
+		tst = true;
+	}
+	else tst = false;
+	return tst;
+}
+
+int testFigure(figure f1)
+{
+	int counter = 3, ctrTest = 0;
+	double Tst[8];
+	for(int i = 0; i < 4; i ++)
+	{
+		int tmp = 0;
+		for(int ctr = 1; ctr < 3; ctr ++)
+		{
+			if( i < 2)
+			{
+				int temp = i + ctr, tmp = 0;
+				Tst[ctrTest] = testPoint(f1.mCoord[counter],f1.mCoord[i],f1.mCoord[temp]);
+				ctrTest++;
+				
+			}
+			else if( i == 2)
+			{
+				int temp = i + ctr;
+				Tst[ctrTest] = testPoint(f1.mCoord[counter],f1.mCoord[i],f1.mCoord[temp]);
+				ctrTest++;
+			}
+			else
+			{
+				int temp = i + ctr - 4;
+				Tst[ctrTest] = testPoint(f1.mCoord[counter],f1.mCoord[i],f1.mCoord[temp]);
+				ctrTest++;
+			}
+		}
+		if(counter == 3)
+		{
+			counter = 0;
+		}
+		else counter++;
+	}
+	std::cout << " " << "\b";//без этого работает не правильно, хз почему.
+	return testArr(Tst);
+}
---
+++ funcshen.hpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#define pi 3.14159
+
+class coord
+{
+	public:
+		double x;
+		double y;
+	coord(double x1=0.0, double y1=0.0): x(x1), y(y1)
+    {
+    }
+    
+	coord operator- (coord v0)
+	{
+		coord v3;
+		v3.x = this->x - v0.x;
+		v3.y = this->y - v0.y;
+		return v3;
+	}
+	friend std::ostream& operator<< (std::ostream &out, const coord &v1)
+	{
+		out << "(x = " << v1.x << " , y = " << v1.y << " )";
+		return out;
+	}
+	coord* initial(double x1, double y1)
+	{
+		this->x = x1;
+		this->y = y1;
+		return this;
+	}
+};
+
+class figure
+{
+	public:
+		coord mCoord[4];
+};
+
+
+int modl(int);//Поиск модуля от int
+
+double modl(double);//Поиск модуля от double
+
+coord vctr(coord, coord);
+
+double testPoint(coord, coord, coord);
+
+int testArr(double *);
+
+int testFigure(figure);
